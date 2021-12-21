bfcf3184180f2ae225d6461f0bf6832ef38061ac
Edd Mann <the@eddmann.com>
2021-12-21 21:15:05 +0000

    Temp: Remove origin request policy

--- stage/apps/website/cloudfront.tf
+++ stage/apps/website/cloudfront.tf
@@ -15,7 +15,6 @@ resource "aws_cloudfront_distribution" "website" {
     cached_methods             = ["GET", "HEAD"]
     viewer_protocol_policy     = "redirect-to-https"
     cache_policy_id            = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
-    origin_request_policy_id   = "216adef6-5c7f-47e4-b989-5492eafa07d3" # AllViewer
     response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03" # SecurityHeadersPolicy
   }
 
