c4de2bf41d03d4fc1456d7c65ebee72db4b955da
Edd Mann <the@eddmann.com>
2021-12-30 23:32:23 +0000

    Add NAT instance public IP output

--- stage/network/outputs.tf
+++ stage/network/outputs.tf
@@ -14,6 +14,10 @@ output "default_security_group_id" {
   value = aws_vpc.main.default_security_group_id
 }
 
+output "nat_instance_ip" {
+  value = aws_eip.nat_instance.public_ip
+}
+
 output "dns_app_zone_ids" {
   value = { for app in var.app_names : (app) => aws_route53_zone.app[app].zone_id }
 }
