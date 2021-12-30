fd7dfb7ec7500c662bd6801b8aa270e583bb3a9e
Edd Mann <the@eddmann.com>
2021-12-30 23:26:36 +0000

    Update Terraform to 1.1.2

--- stage/apps/website/config.tf
+++ stage/apps/website/config.tf
@@ -9,12 +9,12 @@ locals {
 }
 
 terraform {
-  required_version = ">= 1.0.11"
+  required_version = ">= 1.1.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = ">= 3.70.0"
     }
     random = {
       source  = "hashicorp/random"
