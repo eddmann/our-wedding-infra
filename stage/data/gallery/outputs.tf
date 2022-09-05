f6d21e6108f3b8d35a580db68fb02ad3ec574600
Edd Mann <the@eddmann.com>
2022-09-05 17:20:40 +0100

    Revise bucket outputs

--- stage/data/gallery/outputs.tf
+++ stage/data/gallery/outputs.tf
@@ -1,10 +1,16 @@
 output "photo_bucket_arn" {
-  value       = aws_s3_bucket.photo.arn
+  value = {
+    arn         = aws_s3_bucket.photo.arn,
+    domain_name = aws_s3_bucket.photo.bucket_regional_domain_name
+  }
   description = ""
 }
 
-output "upload_bucket_arn" {
-  value       = aws_s3_bucket.upload.arn
+output "upload_bucket" {
+  value = {
+    arn         = aws_s3_bucket.upload.arn,
+    domain_name = aws_s3_bucket.upload.bucket_regional_domain_name
+  }
   description = ""
 }
 
